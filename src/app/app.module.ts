cab67303aa4bf1d7f27a0203be98f3e6fd8468c9
Atul Tripathi <atultripathi@Atuls-MacBook-Air.local>
2020-04-26 09:39:55 +0530

    added markup level truncate for a string using pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardTileComponent } from './card-tile/card-tile.component';
 import { CardListComponent } from './card-list/card-list.component';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardTileComponent,
-    CardListComponent
+    CardListComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -16,7 +16,7 @@ export class CardListComponent implements OnInit {
   ngOnInit(): void {
     this.cardList.push(
       new CardObject('first','first'));
-      this.cardList.push(new CardObject('second','second'));
+      this.cardList.push(new CardObject("Miusov, as a man man of breeding and deilcacy, could not but feel some inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have forgotten himself. Teh monks were not to blame, in any case, he reflceted, on the steps",'second'));
     
 
   }
